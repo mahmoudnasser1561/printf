034b3c26b40cff5e68eb34bc5c2fdc5fa9b3d272
mahmoudnasser1561 <106815734+mahmoudnasser1561@users.noreply.github.com>
2023-11-14 15:12:23 +0200

    finalized

--- print_short_int.c
+++ print_short_int.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * print_int - prints an integer
+ * prinhint - prints a short integer
  * @arguments: input string
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
  * Return: number of chars printed.
  */
-int print_int(va_list arguments, char *buf, unsigned int ibuf)
+int prinhint(va_list arguments, char *buf, unsigned int ibuf)
 {
-	int int_input;
-	unsigned int int_in, int_temp, i, div, isneg;
+	short int int_input;
+	unsigned short int int_in, int_temp, i, div, isneg;
 
 	int_input = va_arg(arguments, int);
 	isneg = 0;
@@ -24,16 +24,13 @@ int print_int(va_list arguments, char *buf, unsigned int ibuf)
 	{
 		int_in = int_input;
 	}
-
 	int_temp = int_in;
 	div = 1;
-
 	while (int_temp > 9)
 	{
 		div *= 10;
 		int_temp /= 10;
 	}
-
 	for (i = 0; div > 0; div /= 10, i++)
 	{
 		ibuf = handl_buf(buf, ((int_in / div) % 10) + '0', ibuf);
