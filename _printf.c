7cb8b2c7445a4cf1739ee201acb5e30805093f09
moharafat <mohamedamr0001@gmail.com>
2023-11-13 22:53:54 -0800

    z

--- _printf.c
+++ _printf.c
@@ -15,10 +15,10 @@ int _printf(const char * const format, ...)
 	};
 
 
-va_list arguments;
-int i = 0, j, len = 0;
+va_list args;
+int i = 0, j = 0, len = 0;
 
-va_start(arguments, format);
+va_start(args, format);
 
 if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 	return (-1);
