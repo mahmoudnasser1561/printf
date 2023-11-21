05557d55ce1aadfdbcc746693d986ad0ac6829a6
moharafat <mohamedamr0001@gmail.com>
2023-11-21 02:03:51 -0800

    z

--- _printf.c
+++ _printf.c
@@ -42,7 +42,7 @@ int _printf(const char *format, ...)
 					str = va_arg(args, char*);
 					while (*str)
 					{
-						putchar(*str);
+						_putchar(*str);
 						counter++;
 						str++;
 					}
